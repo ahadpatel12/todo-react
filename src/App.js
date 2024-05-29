09329f58ec7ad121554dbda21177e34124b71104
ahadpatel12 <ahad.patel@letmegrab.com>
2024-05-29 17:11:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import consumer from './cable';
+import { useState } from 'react';
+
+const todoChannel = consumer.subscriptions.create({
+  channel: 'TodoChannel',
+}, {
+  connected: () => console.log('connected'),
+  disconnected: () => console.log('disconnected'),
+  received: data => console.log(data),
+}
+)
+
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [msg, setMsg] = useState('')
+  // ActionCable.server.
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +28,28 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <input type="text" id='user' value={name} onChange={(e) => setName(e.target.value)} placeholder='User Name' />
+        <input type="text" id='msg' value={msg} onChange={(e) => setMsg(e.target.value)} placeholder='Message' />
+
+        <button
+          onClick={() => {
+
+            todoChannel.send(
+              {
+                sent_by: { name },
+                body: { msg }
+              }
+            )
+          }}
+        > Send message</button>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
---
+++ src/cable.js
@@ -0,0 +1,7 @@
+import { createConsumer } from '@rails/actioncable';
+
+
+const URL = 'ws://localhost:3000/cable';
+const consumer = createConsumer(URL);
+ 
+export default consumer;
